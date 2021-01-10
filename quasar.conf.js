6d6d5d7798585c53d69a359c482cfb718a27da5a
domklip <dominik@screenable.io>
2021-01-10 16:14:24 +0100

    change port and add procfile

--- quasar.conf.js
+++ quasar.conf.js
@@ -80,7 +80,7 @@ module.exports = configure(function(ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8081,
+      port: process.env.PORT || 8081,
       open: true // opens browser window automatically
     },
 
